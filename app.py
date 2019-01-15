ea0771e6d9e0d3f7883edf60448bff00cf82dde5
Hintzen <reh12@ic.ac.uk>
2019-01-15 14:09:03 +0000

    app that runs and shows a single topic through time! Very big data file though. may have to be reduced to only medians and given quantile ranges

--- app.py
+++ app.py
@@ -1,29 +1,76 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import os
+import plotly.graph_objs as go
+import numpy as np
 
 
-app = dash.Dash(__name__)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 server = app.server
 
 
-app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
-])
-
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+colors = {
+    'background': '#ffffff',
+    'text': '#7FDBFF'
+}
 
-@app.callback(dash.dependencies.Output('display-value', 'children'), [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+# Load in your data
+print(dir_path)
+
+df = pd.DataFrame.from_csv(dir_path + '/data/dt_df.csv')
+df['date'] = pd.to_datetime(df['date'])
+df.set_index(df["date"],inplace=True)
+dfplot = df.groupby(pd.Grouper(freq='M')).median()
+dfplot.reset_index(inplace=True)
+dimensions = df.shape
 
 
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hansard Explorer',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Demoing a single topic timeseries', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+	
+	html.H2(children = 'the size of the dataframe is {} rows'.format(dimensions[0]), style = {
+		'textAlign': 'center'
+	}),
+	
+	
+	dcc.Graph(
+        id='topic-through-time',
+        figure={
+            'data': [
+                go.Scatter(
+							x = dfplot['date'],
+							y = np.log10(dfplot['0']),
+							mode = 'lines'
+							)
+							],
+				'layout': go.Layout(
+                xaxis={ 'title': 'Date'},
+                yaxis={'title': 'Probability (log)'}
+				)
+				})
+	
+	])
+	
+	
 if __name__ == '__main__':
-    app.run_server(debug=True)
+	app.run_server(debug=True)
